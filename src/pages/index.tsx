1666e78e193d394c2288ea27d281ac0546156588
urawa72 <urawa72@gmail.com>
2021-06-19 11:50:05 +0900

    add eslint-config-next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const Home: React.VFC<{
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
-                <a href="#">{title}</a>
+                <a>{title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
@@ -45,47 +45,6 @@ const Home: React.VFC<{
     </Layout>
   );
 };
-
-// export default function Home({
-//   allPostsData,
-// }: {
-//   allPostsData: {
-//     date: string;
-//     title: string;
-//     id: string;
-//   }[];
-// }) {
-//   return (
-//     <Layout home>
-//       <Head>
-//         <title>{siteTitle}</title>
-//       </Head>
-//       <section className={utilStyles.headingMd}>
-//         <p>[Your Self Introduction]</p>
-//         <p>
-//           (This is a sample website - you’ll be building a site like this in{' '}
-//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-//         </p>
-//       </section>
-//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-//         <h2 className={utilStyles.headingLg}>Blog</h2>
-//         <ul className={utilStyles.list}>
-//           {allPostsData.map(({ id, date, title }) => (
-//             <li className={utilStyles.listItem} key={id}>
-//               <Link href={`/posts/${id}`}>
-//                 <a>{title}</a>
-//               </Link>
-//               <br />
-//               <small className={utilStyles.lightText}>
-//                 <Date dateString={date} />
-//               </small>
-//             </li>
-//           ))}
-//         </ul>
-//       </section>
-//     </Layout>
-//   );
-// }
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
